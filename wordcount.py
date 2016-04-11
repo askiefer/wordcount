65990924343e6d593ff20222516897666f2f21dd
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 12:41:11 -0700

    lower case, punct out

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,28 @@
 
+dictionary = {}
+
+def add_word_to_dict(word):
+    if word in dictionary:
+        dictionary[word] += 1
+    else:
+        dictionary[word] = 1
+
 def word_count(file):
-    """"""
+    """Counts the number of times a word appears in a file"""
     with open(file) as file_in_use:
-        dictionary = {}
+        
         # iterates through lines in file
         for line in file_in_use:
             # iterates through words in list line
             for word in line.split():
+                word = word.lower() 
                 # adds word to dictionary at count = 1, or ups the count
-                if word in dictionary:
-                        dictionary[word] += 1
+                # checks for punctuation 
+                if word.isalpha():
+                    add_word_to_dict(word) 
                 else:
-                    dictionary[word] = 1
+                    word = word[:-1]
+                    add_word_to_dict(word)
     print dictionary
 
 word_count("test.txt")
